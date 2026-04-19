1223ba2ef21990bb6ee143780065529bdc79fbf9
lipebaba <felipebabateixeira@hotmail.com>
2026-04-19 18:46:07 -0300

    Código

--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Tarefas
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas = Tarefas.objects.all().values()
+    return JsonResponse(list(tarefas), safe=False)
+
+def listar_tarefas_abertas(request):
+    tarefas = Tarefas.objects.filter(status='ABERTA').values()
+    return JsonResponse(list(tarefas), safe=False)
